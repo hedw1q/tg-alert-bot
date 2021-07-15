eeba5156d208ec845c70ee5f30da132ecaf27dcc
Ainur <ryamovainur@gmail.com>
2021-07-15 18:45:11 +0300

    pingApp method

--- src/main/java/ru/hedw1q/TgBot/TgBotApplication.java
+++ src/main/java/ru/hedw1q/TgBot/TgBotApplication.java
@@ -2,9 +2,19 @@ package ru.hedw1q.TgBot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
+@Controller
 public class TgBotApplication {
+
+	@RequestMapping("/")
+	@ResponseBody
+	String pingApp() {
+		return "Hello World!";
+	}
 
 	public static void main(String[] args){
 			SpringApplication.run(TgBotApplication.class, args);
