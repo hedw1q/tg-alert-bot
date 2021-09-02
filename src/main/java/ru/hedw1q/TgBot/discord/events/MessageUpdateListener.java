19b000fd282afc0ae5312ae12650f5ff879e755d
Ainur <ryamovainur@gmail.com>
2021-09-02 20:50:04 +0300

    discord message update handler added

--- src/main/java/ru/hedw1q/TgBot/discord/events/MessageUpdateListener.java
+++ src/main/java/ru/hedw1q/TgBot/discord/events/MessageUpdateListener.java
@@ -1,9 +1,18 @@
 package ru.hedw1q.TgBot.discord.events;
 
+import discord4j.core.event.domain.message.MessageCreateEvent;
 import discord4j.core.event.domain.message.MessageUpdateEvent;
+import discord4j.core.object.entity.Attachment;
 import discord4j.core.object.entity.Message;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
+import ru.hedw1q.TgBot.discord.entities.DiscordMessage;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static ru.hedw1q.TgBot.discord.DsBotInitializer.stickerMap;
+import static ru.hedw1q.TgBot.discord.events.MessageCreateListener.*;
 
 /**
  * @author hedw1q
@@ -18,10 +27,34 @@ public class MessageUpdateListener extends MessageListener implements EventListe
 
     @Override
     public Mono<Void> execute(MessageUpdateEvent event) {
-       Message oldMessage =event.getOld().orElseThrow();
-
-
+        if (!validateMessage(event)) {
             return Mono.empty();
+        }
 
+
+        DiscordMessage discordMessage = new DiscordMessage("UPD: "+event.getMessage().block().getContent());
+        discordMessage.formatOutputText(stickerMap);
+
+        if (!event.getMessage().block().getAttachments().isEmpty()) {
+            List<Attachment> attachmentList = new ArrayList<>(event.getMessage().block().getAttachments());
+            attachmentList.stream()
+                    .map(Attachment::getUrl)
+                    .forEach(attachmentUrl -> tgBot.sendAttachmentMessageToChannel(TG_CHANNEL_ID, attachmentUrl, discordMessage.getMessageText()));
+            return Mono.empty();
+        }
+
+        tgBot.sendTextMessageToChannel(TG_CHANNEL_ID, discordMessage.getMessageText());
+
+        return Mono.empty();
+
+    }
+
+
+    private boolean validateMessage(MessageUpdateEvent event) {
+        if (!DS_CHANNEL_IDS.contains(event.getChannelId().asLong())
+                || event.getMessage().block().getAuthor().orElseThrow().isBot()
+                || !event.getMessage().block().getAuthor().orElseThrow().getUsername().trim().equals("hedw1q"))
+            return false;
+        return true;
     }
 }
