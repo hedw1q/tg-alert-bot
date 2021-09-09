2699cde95bd8bbe66f10c1354dfc2de0c149b6c2
Ainur <ryamovainur@gmail.com>
2021-09-09 10:56:50 +0300

    helix fix

--- src/main/java/ru/hedw1q/TgBot/twitch/TwBot.java
+++ src/main/java/ru/hedw1q/TgBot/twitch/TwBot.java
@@ -69,6 +69,7 @@ public class TwBot {
                 .withClientId(twitchConfiguration.getClientId())
                 .withClientSecret(twitchConfiguration.getClientSecret())
                 .withEnableChat(true)
+                .withEnableHelix(true)
                 .withEnablePubSub(true)
                 .withChatAccount(credential)
                 .build();
